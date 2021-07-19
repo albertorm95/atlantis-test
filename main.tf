2b683f1f2aa7f181ce060818cb38b091bed9285d
Alberto Rojas <alberto.rojas@n26.com>
2021-07-19 14:40:38 +0200

    test

--- main.tf
+++ main.tf
@@ -1,5 +1,14 @@
 resource "null_resource" "example2" {
   provisioner "local-exec" {
+<<<<<<< Updated upstream
     command = "echo tess"
+=======
+    command = "Get-Date > completed.txt"
+    interpreter = ["PowerShell", "-Command"]
+>>>>>>> Stashed changes
   }
+}
+
+module "test" {
+  source = "./module"
 }
