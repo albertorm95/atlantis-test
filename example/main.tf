15bf0ad421a9aec0376a03c18d7d9b6c75b18c62
Alberto Rojas <alberto.rojas@n26.com>
2021-07-19 15:35:08 +0200

    fix

--- example/main.tf
+++ example/main.tf
@@ -4,9 +4,22 @@ variable "env" {
 variable "byte_length" {
   type = number
 }
+variable "create_before_destroy" {
+  type = bool
+}
 
-module "test" {
+module "test_live" {
+  count = var.env == "live" ? 1 : 0
   source = "../"
   env = var.env
   byte_length = var.byte_length
+  create_before_destroy = var.create_before_destroy
+}
+
+module "test_dev" {
+  count = var.env == "dev" ? 1 : 0
+  source = "../"
+  env = var.env
+  byte_length = var.byte_length
+  create_before_destroy = var.create_before_destroy
 }
--- main.tf
+++ main.tf
@@ -4,8 +4,14 @@ variable "env" {
 variable "byte_length" {
   type = number
 }
+variable "create_before_destroy" {
+  type = bool
+}
 
 resource "random_id" "suffix" {
   prefix = var.env
   byte_length = var.byte_length
+  lifecycle {
+    create_before_destroy = var.create_before_destroy
+  }
 }
