442064beccf4a5e1b829287b7bbabaea6db28c96
Alberto Rojas <alberto.rojas@n26.com>
2021-07-19 15:16:03 +0200

    fix

--- example/main.tf
+++ example/main.tf
@@ -1,13 +1,3 @@
-terraform {
-  backend "s3" {
-    encrypt        = true
-    bucket         = "n26-atlantis-demo-states"
-    dynamodb_table = "atlantis-demo-states-lock"
-    region         = "eu-central-1"
-    role_arn       = "arn:aws:iam::576985140337:role/fortknox26/ControlPlaneAccountAccessRole"
-  }
-}
-
 variable "evn" {
   type = "string"
 }
