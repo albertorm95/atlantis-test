7de8a393bd2902081f255a455508f7edfe22d129
Alberto Rojas <alberto.rojas@n26.com>
2021-07-19 15:10:35 +0200

    fix

--- main.tf
+++ main.tf
@@ -2,8 +2,6 @@ variable "env" {
   type = "string"
 }
 
-resource "null_resource" "example" {
-  provisioner "local-exec" {
-    command = "echo ${var.env}"
-  }
+resource "random_id" "suffix" {
+  byte_length = 4
 }
