760fec19a88bbd33082a04d0af40a9cbaa9d95e4
Alberto Rojas <alberto.rojas@n26.com>
2021-07-19 14:53:40 +0200

    base

---
+++ example/backends/live.tf
@@ -0,0 +1,5 @@
+terraform {
+  backend "local" {
+    path = "/Users/albertorojas/Projects/Personal/local-atlantis/atlantis-test-state/live-terraform.tfstate"
+  }
+}
--- main.tf
+++ main.tf
@@ -1,3 +1,7 @@
+variable "evn" {
+  type = "string"
+}
+
 resource "null_resource" "example" {
   provisioner "local-exec" {
     command = "echo ${var.env}"
