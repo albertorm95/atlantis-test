c387e80a650b9283ba0677e6cdc61c73a12ecbfc
Alberto Rojas <alberto.rojas@n26.com>
2021-07-19 15:36:03 +0200

    fix

--- example/example.tfvars
+++
@@ -1,1 +0,0 @@
-create_before_destroy = false
--- example/main.tf
+++ example/main.tf
@@ -4,16 +4,12 @@ variable "env" {
 variable "byte_length" {
   type = number
 }
-variable "create_before_destroy" {
-  type = bool
-}
 
 module "test_live" {
   count = var.env == "live" ? 1 : 0
   source = "../"
   env = var.env
   byte_length = var.byte_length
-  create_before_destroy = var.create_before_destroy
 }
 
 module "test_dev" {
@@ -21,5 +17,4 @@ module "test_dev" {
   source = "../"
   env = var.env
   byte_length = var.byte_length
-  create_before_destroy = var.create_before_destroy
 }
